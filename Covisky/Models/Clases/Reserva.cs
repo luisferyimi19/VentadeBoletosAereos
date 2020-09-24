ffa5efc8aa435d38cb378ae1a56334c13c1e0119
csalazarh14 <csalazarh7@gmail.com>
2020-09-23 22:05:36 -0600

    Proyecto

---
+++ Covisky/Models/Clases/Reserva.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Covisky.Models.Clases
+{
+    public class Reserva
+    {
+        public string origen { get; set; }
+        public string destino { get; set; }
+        public DateTime sale { get; set; }
+        public DateTime regresa { get; set; }
+        public int cant_adulto { get; set; }
+        public int cant_nino { get; set; }
+        public int cant_bebe { get; set; }
+        public string clase { get; set; }
+    }
+}
