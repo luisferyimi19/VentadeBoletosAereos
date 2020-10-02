4d4a9cd1dca9b062957d14f2500674219668f5bc
Luis <ldavilad@miumg.edu.gt>
2020-10-02 11:56:14 -0600

    Fix home

--- Covisky/Scripts/tabmenu.js
+++ Covisky/Scripts/tabmenu.js
@@ -33,3 +33,39 @@ function showPanelCheck(panelIndexCheck, colorCode) {
     tabPanelsCheck[panelIndexCheck].style.backgroundColor = colorCode;
 }
 showPanelCheck(0, 'rgba(0, 0, 0, 0)');
+
+var tabButtonsFly = document.querySelectorAll(".tabContainerFly .buttonContainerFly input");
+var tabPanelsFly = document.querySelectorAll(".tabContainerFly  .tabPanelFly");
+
+function showPanelFly(panelIndexFly, colorCode) {
+    tabButtonsFly.forEach(function (node) {
+        node.style.backgroundColor = "";
+        node.style.color = "";
+    });
+    tabButtonsFly[panelIndexFly].style.backgroundColor = "white";
+    tabButtonsFly[panelIndexFly].style.color = "black";
+    tabPanelsFly.forEach(function (node) {
+        node.style.display = "none";
+    });
+    tabPanelsFly[panelIndexFly].style.display = "block";
+    tabPanelsFly[panelIndexFly].style.backgroundColor = colorCode;
+}
+showPanelFly(0, 'rgba(0, 0, 0, 0)');
+
+var tabButtonsReserve = document.querySelectorAll(".tabContainerReserve .buttonContainerReserve input");
+var tabPanelsReserve = document.querySelectorAll(".tabContainerReserve  .tabPanelReserve");
+
+function showPanelReserve(panelIndexReserve, colorCode) {
+    tabButtonsReserve.forEach(function (node) {
+        node.style.backgroundColor = "";
+        node.style.color = "";
+    });
+    tabButtonsReserve[panelIndexReserve].style.backgroundColor = "white";
+    tabButtonsReserve[panelIndexReserve].style.color = "black";
+    tabPanelsReserve.forEach(function (node) {
+        node.style.display = "none";
+    });
+    tabPanelsReserve[panelIndexReserve].style.display = "block";
+    tabPanelsReserve[panelIndexReserve].style.backgroundColor = colorCode;
+}
+showPanelReserve(0, 'rgba(0, 0, 0, 0)');
