aa491a24b2df3297e9b69ca6a07104ee83174093
Luis <ldavilad@miumg.edu.gt>
2020-10-01 19:43:51 -0600

    Fix styles

--- Covisky/Scripts/tabmenu.js
+++ Covisky/Scripts/tabmenu.js
@@ -15,3 +15,21 @@ function showPanel(panelIndex, colorCode) {
     tabPanels[panelIndex].style.backgroundColor = colorCode;
 }
 showPanel(0, 'rgba(0, 0, 0, 0)');
+
+var tabButtonsCheck = document.querySelectorAll(".tabContainerCheck .buttonContainerCheck input");
+var tabPanelsCheck = document.querySelectorAll(".tabContainerCheck  .tabPanelCheck");
+
+function showPanelCheck(panelIndexCheck, colorCode) {
+    tabButtonsCheck.forEach(function (node) {
+        node.style.backgroundColor = "";
+        node.style.color = "";
+    });
+    tabButtonsCheck[panelIndexCheck].style.backgroundColor = "white";
+    tabButtonsCheck[panelIndexCheck].style.color = "black";
+    tabPanelsCheck.forEach(function (node) {
+        node.style.display = "none";
+    });
+    tabPanelsCheck[panelIndexCheck].style.display = "block";
+    tabPanelsCheck[panelIndexCheck].style.backgroundColor = colorCode;
+}
+showPanelCheck(0, 'rgba(0, 0, 0, 0)');
