add42092c87ba6c654e3fa7de19e1af63f886039
csalazarh14 <csalazarh7@gmail.com>
2020-10-31 09:08:14 -0600

    update

---
+++ Covisky/Models/Clases/Informacion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Covisky.Models.Clases
+{
+    public class Informacion
+    {
+        public string apellido { get; set; }
+        public string nombre { get; set; }
+        public DateTime fechanac { get; set; }
+        public string correo { get; set; }
+        public string confirmarcorreo { get; set; }
+        public int telefonoinfo { get; set; }
+    }
+}
---
+++ Covisky/Models/Clases/Pagos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Covisky.Models.Clases
+{
+    public class Pagos
+    {
+        public int notarjeta { get; set; }
+        public string fechaven { get; set; }
+        public int cvv { get; set; }
+        public string nombretar { get; set; }
+        public string pasaporte { get; set; }
+        public long nopasaporte { get; set; }
+        public string correotar { get; set; }
+        public string confirmartar { get; set; }
+        public string direccion { get; set; }
+        public string pais { get; set; }
+        public string estado { get; set; }
+        public string ciudad { get; set; }
+        public int codigop { get; set; }
+        public int telefonotar { get; set; }
+    }
+}
